78bfd0a3e9eacb8817cbb66cb50f82d0e947cace
jiacai2050 <dev@liujiacai.net>
2024-02-06 22:40:40 +0800

    fix go-demo for macos

--- tests/run-go.sh
+++ tests/run-go.sh
@@ -9,6 +9,12 @@ export CGO_ENABLED=1
 export CC="zigcc"
 export CXX="zigcxx"
 
+if command -v xcrun;then
+  # https://github.com/ziglang/zig/issues/10790
+  # https://stackoverflow.com/a/68766210
+  export ZIGCC_FLAGS="-I/usr/include -F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/ -L/usr/lib --sysroot=$(xcrun --show-sdk-path)"
+fi
+
 cd "${ROOT_DIR}/go-demo" && go build -ldflags="-v"
 
 if [ $? -eq 0 ]; then
